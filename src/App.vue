42d84ed33473733516d5c86d84143052bcf2a88d
Julian Valle <julian.valle@enovational.com>
2022-05-03 20:03:18 -0400

    set up front end for the form May separate the cards into their own components from the form.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1 class="app-title">Quiz Application</h1>
+    </header>
+    <main>
+      <h1 class="quiz-title">Quiz 1 - HTML / CSS / JS Practice</h1>
+
+      <QuestionCard :cards="cards" />
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuestionCard from "./components/QuestionCard.vue";
+import questions from "../questions.json";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { QuestionCard },
+  data() {
+    return {
+      cards: questions,
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html {
+  font-size: 16px;
+}
+main {
+  max-width: 1080px;
+  margin: 0rem auto;
+  padding: 1rem;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "montserrat", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.app-title {
+  font-size: 1.25rem;
+  padding: 1rem 1.5rem;
+  background-color: #721121;
+  color: #ffffff;
+  font-weight: 300;
+}
+.quiz-title {
+  font-weight: 400;
+  padding-bottom: 1rem;
 }
 </style>
---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <form @submit="console.log(picked)">
+    <div class="card" v-for="card in cards" :key="card.id">
+      <h3 class="question">{{ card.id }}. {{ card.question }}</h3>
+      <div class="options">
+        <div class="answer">
+          <input
+            type="radio"
+            value="A"
+            :name="`choices-${card.id}`"
+            v-model="pickedA"
+          />
+          <label for="A">{{ card.options[0] }}</label>
+        </div>
+        <div class="answer">
+          <input
+            type="radio"
+            value="B"
+            :name="`choices-${card.id}`"
+            v-model="picked"
+          />
+          <label for="B">{{ card.options[1] }}</label>
+        </div>
+        <div class="answer">
+          <input
+            type="radio"
+            value="C"
+            :name="`choices-${card.id}`"
+            v-model="picked"
+          />
+          <label for="C">{{ card.options[2] }}</label>
+        </div>
+        <div class="answer">
+          <input
+            type="radio"
+            value="D"
+            :name="`choices-${card.id}`"
+            v-model="picked"
+          />
+          <label for="D">{{ card.options[3] }}</label>
+        </div>
+      </div>
+    </div>
+
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    cards: Array,
+  },
+  data() {
+    return { picked: "", pickedA: "a" };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  padding: 1rem 1.5rem;
+  border-radius: 0.5rem;
+  -webkit-box-shadow: 0px 1px 7px -3px #000000;
+  box-shadow: 0px 1px 7px -3px #000000;
+  margin-bottom: 2rem;
+
+  label {
+    padding: 0 1rem;
+  }
+}
+.question {
+  font-weight: 400;
+  padding-bottom: 1rem;
+}
+
+.options {
+  padding: 0 1rem;
+  .answer {
+    padding-bottom: 0.5rem;
+  }
+}
+</style>
